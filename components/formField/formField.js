4d253c46283068dff41a8cff931a255074f9efa3
JanUlc <sru799@gmail.com>
2022-04-16 21:04:19 +0200

    Error: Component definition is missing display name  react/display-name

--- components/formField/formField.js
+++ components/formField/formField.js
@@ -1,20 +1,20 @@
 import React from "react";
 import {Wrapper, StyledInput} from './FormField.style';
 
-const FormField = React.forwardRef(
-  ({ onChange, name, id, placeholder, ...props }, ref) => {
-    return (
-      <Wrapper>
-        <StyledInput
-          name={name}
-          id={id}
-          {...props}
-          ref={ref}
-          placeholder={placeholder}
-        />
-      </Wrapper>
-    )
-  }
-);
-
-export default FormField;
+export default function FormField() {
+  return (React.forwardRef(
+    ({ onChange, name, id, placeholder, ...props }, ref) => {
+      return (
+        <Wrapper>
+          <StyledInput
+            name={name}
+            id={id}
+            {...props}
+            ref={ref}
+            placeholder={placeholder}
+          />
+        </Wrapper>
+      )
+    }
+  ))
+} ;
